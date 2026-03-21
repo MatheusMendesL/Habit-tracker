44943f5aa74e4a32f9767a3a45dbc2fd9377594c
MatheusMendesL <matheusmendelopes@gmail.com>
2026-03-21 14:54:38 -0300

    adding some zaps, I will search more about it

--- backend/services/user-service/cmd/main.go
+++ backend/services/user-service/cmd/main.go
@@ -9,31 +9,50 @@ import (
 	"user-service/internal/repository"
 	"user-service/internal/service"
 
+	"github.com/grpc-ecosystem/go-grpc-middleware"
+	grpcZap "github.com/grpc-ecosystem/go-grpc-middleware/logging/zap"
+	grpcRecovery "github.com/grpc-ecosystem/go-grpc-middleware/recovery"
+	"go.uber.org/zap"
 	"google.golang.org/grpc"
 )
 
 func main() {
 	log.Println("Starting server...")
+
+	if err := startServer(); err != nil {
+		panic(err)
+	}
+}
+
+func startServer() (err error) {
 	list, err := net.Listen("tcp", ":8080")
 
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
-
 	dbConn, queries, err := db.Conn()
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 	defer dbConn.Close()
 
 	userRepo := repository.NewUserRepository(queries)
 	userService := service.NewUserService(userRepo)
 	userHandler := handler.NewUserHandler(userService)
-	grpcServer := grpc.NewServer()
+	logger, _ := zap.NewProduction()
+	defer logger.Sync()
+	grpcServer := grpc.NewServer(
+		grpc.UnaryInterceptor(grpc_middleware.ChainUnaryServer(
+			grpcRecovery.UnaryServerInterceptor(),
+			grpcZap.UnaryServerInterceptor(logger),
+		)),
+	)
 
 	pb.RegisterUserServiceServer(grpcServer, userHandler)
 
 	if err := grpcServer.Serve(list); err != nil {
-		log.Fatal(err)
+		return err
 	}
+
+	return nil
 }
