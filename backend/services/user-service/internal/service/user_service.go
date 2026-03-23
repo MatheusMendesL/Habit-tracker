7fd3e45f02c1a997c95f5a9473cdce8233315422
MatheusMendesL <matheusmendelopes@gmail.com>
2026-03-23 10:40:47 -0300

    added 4 methods of following

--- backend/services/user-service/internal/service/user_service.go
+++ backend/services/user-service/internal/service/user_service.go
@@ -3,6 +3,7 @@ package service
 import (
 	"context"
 	"user-service/db"
+	AppErr "user-service/internal/errors"
 	"user-service/internal/repository"
 )
 
@@ -28,4 +29,24 @@ func (s *UserService) DeleteUser(ctx context.Context, id int32) error {
 
 func (s *UserService) UpdateUser(ctx context.Context, user db.UpdateUserParams) (*db.User, error) {
 	return s.repo.UpdateUser(ctx, user)
+}
+
+func (s *UserService) StartFollowing(ctx context.Context, followerId int32, followeeId int32) error {
+	if followerId == followeeId {
+		return AppErr.ErrInvalidArgument
+	}
+
+	return s.repo.StartFollowing(ctx, followerId, followeeId)
+}
+
+func (s *UserService) StopFollowing(ctx context.Context, followerId int32, followeeId int32) error {
+	return s.repo.StopFollowing(ctx, followerId, followeeId)
+}
+
+func (s *UserService) ListFollowers(ctx context.Context, userId int32) ([]*db.User, error) {
+	return s.repo.ListFollowers(ctx, userId)
+}
+
+func (s *UserService) ListFollowing(ctx context.Context, userId int32) ([]*db.User, error) {
+	return s.repo.ListFollowing(ctx, userId)
 }
