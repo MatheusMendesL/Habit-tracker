492a4b685a9554ce38f58746910f13a25ad839bd
MatheusMendesL <matheusmendelopes@gmail.com>
2026-04-03 17:25:24 -0300

    feat: Habit-service added the method CreateHabit

--- backend/services/habit-service/internal/utils/utils.go
+++ backend/services/habit-service/internal/utils/utils.go
@@ -1,6 +1,10 @@
 package utils
 
-import "database/sql"
+import (
+	"database/sql"
+	"habit-service/db"
+	pbHabit "shared/pb/habit"
+)
 
 func ToNullString(param string) sql.NullString {
 	return sql.NullString{
@@ -14,4 +18,14 @@ func NullStringToString(ns sql.NullString) string {
 		return ns.String
 	}
 	return ""
+}
+
+func ToProtoHabit(habit db.Habit) *pbHabit.Habit {
+	return &pbHabit.Habit{
+		Id:          habit.ID,
+		UserId:      habit.UserID,
+		Name:        habit.Name,
+		Description: NullStringToString(habit.Description),
+		ImageUrl:    NullStringToString(habit.ImageUrl),
+	}
 }
