5f7a289ba3338fc86891704b2bc587d58c127303
MatheusMendesL <matheusmendelopes@gmail.com>
2026-07-03 23:42:29 -0300

    fix: add a new controller method to test the nginx

--- backend/auth-services-node/src/routers/user_router.js
+++ backend/auth-services-node/src/routers/user_router.js
@@ -8,5 +8,6 @@ router.post("/login", functionsControllers.login)
 router.post("/signup", functionsControllers.signup)
 router.get("/get_user_data/", jwt, functionsControllers.get_user_data)
 router.post("/logout", jwt, functionsControllers.logout)
+router.get("/data/:id", functionsControllers.get_user_data_test)
 
 module.exports = router;
